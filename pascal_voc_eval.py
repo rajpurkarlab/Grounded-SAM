418eb360394f8732a1c9bd5e8dd1d9df0b30f854
Vignav Ramesh <rvignav@gmail.com>
2023-07-20 04:24:57 -0400

    Updaate

--- pascal_voc_eval.py
+++ pascal_voc_eval.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import json
 
 from pascal_utils import get_queries
 from grounded_sam import run_grounded_sam, env_setup, load_models
@@ -40,15 +41,14 @@ def pascal_voc_eval():
     groundingdino_model, sam_predictor = load_models()
 
     # Evaluation
-    ctr = 0
-    iou_sum = 0
+    iou_results = {class_name: [] for class_name in class_names}
     for id in val_ids:
         # load image
         img_path = img_folder_path + '/' + id + '.jpg'
 
         # load ground truth
         gt_path = gt_folder_path + '/' + id + '.png'
-        gt_masks = get_queries(gt_path) # to be implemented by vignav -> ["text_prompt": "aeroplane", "gt_mask": np_array]
+        gt_masks = get_queries(gt_path)
 
         # run through all classes
         for class_name in class_names:
@@ -60,15 +60,34 @@ def pascal_voc_eval():
 
             # run grounded sam
             text_prompt = 'a ' + class_name
-            pred_mask = run_grounded_sam(img_path, text_prompt, groundingdino_model, sam_predictor) # to be implemented by vignav -> np_array
-            pred_mask = (pred_mask != 0).astype(int)
 
-            # compute iou
-            iou_score = get_iou(pred_mask, gt_mask)
-            print(id, class_name, iou_score)
-            iou_sum += iou_score
-            ctr += 1
-    mIoU = iou_sum / ctr
+            try:
+                pred_mask = run_grounded_sam(img_path, text_prompt, groundingdino_model, sam_predictor)
+                pred_mask = (pred_mask != 0).astype(int)
+
+                # compute iou
+                iou_score = get_iou(pred_mask, gt_mask)
+                print(id, class_name, iou_score) 
+                
+                iou_results[class_name].append(iou_score)
+            except:
+                print(f"\nSkipping {img_path}, {text_prompt} due to errors\n")
+
+    # compute average mIoU across all classes
+    total_sum = 0
+    total_count = 0
+    for value_list in iou_results.values():
+        total_sum += sum(value_list)
+        total_count += len(value_list)
+    mIoU = total_sum / total_count
+
+    # compute mIoU by class and save to json
+    mIoU_classes = {}
+    for class_name in class_names:
+        mIoU_classes[class_name] = np.mean(iou_results[class_name])
+    mIoU_classes['mIoU'] = mIoU
+    json.dump(mIoU_classes, open('pascal_miou.json', 'w'))
+    
     return mIoU
         
 
