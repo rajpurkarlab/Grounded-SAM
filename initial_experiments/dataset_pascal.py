8e39368c3f403153e6f7b777c616fa3ede59acd4
martinzwm <martinma@g.havard.edu>
2023-10-05 11:25:58 -0400

    Regular backup - there is an error when running test case @Vignav

--- initial_experiments/dataset_pascal.py
+++ initial_experiments/dataset_pascal.py
@@ -70,8 +70,8 @@ class PASCALDataset(Dataset):
                 # add ground truth segmentation
                 inputs["ground_truth_mask"] = ground_truth_mask
                 
-                # inputs["image"] = img
-                # inputs["image_path"] = img_path
+                inputs["image"] = img
+                inputs["image_path"] = img_path
                 
                 self.samples.append(inputs)
             
@@ -84,28 +84,95 @@ class PASCALDataset(Dataset):
                 
         return self.samples[idx]
 
+
+# class PASCALDataset(Dataset):
+#     """PASCAL VOC dataset."""
+
+#     def __init__(self, size=(256,256)):
+#         """
+#         tensors (boolean): If true, return image as tensor. If false, don't return image (much faster).
+#         """
+#         self.train_id_path = '/n/data1/hms/dbmi/rajpurkar/lab/Grounded-SAM/datasets/pascal/VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt'
+#         self.class_name_path = '/n/data1/hms/dbmi/rajpurkar/lab/Grounded-SAM/datasets/pascal/VOCdevkit/VOC2012/ImageSets/Segmentation/class_names.txt'
+#         self.img_folder_path = '/n/data1/hms/dbmi/rajpurkar/lab/Grounded-SAM/datasets/pascal/VOCdevkit/VOC2012/JPEGImages'
+#         self.gt_folder_path = '/n/data1/hms/dbmi/rajpurkar/lab/Grounded-SAM/datasets/pascal/VOCdevkit/VOC2012/SegmentationClass'
+#         self.processor = processor = SamProcessor.from_pretrained("facebook/sam-vit-base")
+    
+#         # Load class names
+#         self.class_names = []
+#         for line in open(self.class_name_path, 'r'):
+#             self.class_names.append(line.strip())
+        
+#         # Load val ids
+#         self.train_ids = []
+#         for line in open(self.train_id_path, 'r'):
+#             id = line.strip()
+#             self.train_ids.append(id)
+        
+#         self.size = size
+        
+            
+#     def __len__(self):
+#         return len(self.train_ids)
+
+
+#     def __getitem__(self, idx):
+#         if torch.is_tensor(idx):
+#             idx = idx.tolist()
+
+#         id = self.train_ids[idx]
+#         img_path = self.img_folder_path + '/' + id + '.jpg'
+
+#         gt_path = self.gt_folder_path + '/' + id + '.png'
+        
+#         gt_masks = get_queries(gt_path, self.size)
+
+#         img = Image.open(img_path)
+#         img = img.resize(self.size)
+                        
+#         for val in gt_masks:
+#             ground_truth_mask = gt_masks[val]
+            
+#             prompt = get_bounding_box(ground_truth_mask)
+        
+#             # prepare image and prompt for the model
+#             inputs = self.processor(img, input_boxes=[[prompt]], return_tensors="pt")
+
+#             # remove batch dimension which the processor adds by default
+#             inputs = {k:v.squeeze(0) for k,v in inputs.items()}
+
+#             # add ground truth segmentation
+#             inputs["ground_truth_mask"] = ground_truth_mask
+            
+#             inputs["image"] = img
+#             inputs["image_path"] = img_path
+                
+#         return inputs
+
+
 def load_data(batch_size=16, num_workers=0):
     """Get dataloader for training.
     """
     dataset = PASCALDataset()
-    return DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    l = len(dataset)
+    return l, DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
-def get_len():
-    dataset = PASCALDataset()
-    return len(dataset)
 
 class UnitTest:
     def __init__(self):
         pass
 
     def load_data_test(self):
-        dataloader = load_data()
+        print("Loading PASCAL dataset (taking a long time due to preprocessing being done at initialization)...")
+        num_samples, dataloader = load_data()
+        print("Finished loading PASCAL dataset.")
 
         print("Number of batches:", len(dataloader))
         for i, data in enumerate(dataloader):
             images = data["image"]
             masks = data["gt_mask"]
         print("Passed all tests")
+
 
 if __name__=='__main__':
     unit_test = UnitTest()
